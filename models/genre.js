a2e5c060892dc8fa12c8604607741c04aa130782
Quoc Viet <nqviet157@gmail.com>
2019-09-09 10:32:06 +0700

    rebuild

--- models/genre.js
+++ models/genre.js
@@ -3,24 +3,28 @@ const mongoose = require('mongoose');
 
 //Set up the schema - shape of a document
 const genreSchema = new mongoose.Schema({
-    id: Number,
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255
-    }
+  id: Number,
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+  }
 });
 
 //Connect to a collection
 const Genres = mongoose.model('genre', genreSchema);
 
-function genreValidate(genre) {
-    const schema = {
-        name: Joi.string().required().min(5).max(255)
-    }
+Genres.prototype.show = function () {
+  console.log('It works.');
+}
 
-    return Joi.validate(genre, schema);
+function genreValidate(genre) {
+  const schema = {
+    name: Joi.string().required().min(5).max(255)
+  }
+
+  return Joi.validate(genre, schema);
 }
 
 exports.Genres = Genres;
