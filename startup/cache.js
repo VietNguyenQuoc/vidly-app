17ff5bfd784a6601d6dddae1f17278a3f718daef
Quoc Viet <nqviet157@gmail.com>
2019-09-09 12:27:38 +0700

    cache

--- startup/cache.js
+++ startup/cache.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const config = require('config');
-const redisUrl = config.get('redis_url');
-const client = redis.createClient(process.env.REDIS_URL);
+const url = require('url');
+const redisUrl = url.parse(process.env.REDIS_URL);
+const client = redis.createClient(redisUrl.port, redisUrl.hostname, { no_ready_check: true });
 const util = require('util');
 client.get = util.promisify(client.get);
 
